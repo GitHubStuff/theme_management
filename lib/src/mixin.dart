07a43e9e3e48ff9435ab8e80d7f4d0f2387a8f44
Steven Smith <github@icodeforyou.com>
2022-01-20 22:06:21 -0500

    OCD fix on mixin name

--- lib/src/mixin.dart
+++ lib/src/mixin.dart
@@ -5,6 +5,26 @@ import '../theme_management.dart';
 
 typedef BodyWidget = Widget Function(BuildContext context);
 
+mixin ThemeMixin {
+  /// Place in the widget tree for child branches to respond to language changes (or any other cubit),
+  /// and change from Dark/Light themes.
+  /// NOTE: a typical place for this is right below the scaffold, eg: Scaffold(..,body: themeAndLocale)
+  Widget rebuildTreeOnThemeOrCubit<T>({
+    Cubit<T>? cubit,
+    required BodyWidget bodyWidget,
+  }) =>
+      (cubit == null)
+          ? BlocBuilder(bloc: ThemeManagement.themeModeCubit, builder: (context, _) => bodyWidget(context))
+          : BlocBuilder(
+              bloc: cubit,
+              builder: (_, __) => BlocBuilder(
+                bloc: ThemeManagement.themeModeCubit,
+                builder: (context, _) => bodyWidget(context),
+              ),
+            );
+}
+
+@Deprecated('Now called "ThemeMixin"')
 mixin themeMixin {
   /// Place in the widget tree for child branches to respond to language changes (or any other cubit),
   /// and change from Dark/Light themes.
